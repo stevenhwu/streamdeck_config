fe606a29125200d2d65dbcf41727f7a7016f2173
Steven Wu <stevenhwu@gmail.com>
2024-05-03 11:34:02 +0800

    update sync and meeting

---
+++ scripts/main_meeting.sh
@@ -0,0 +1,1 @@
+google-chrome --new-window --host  https://github.com/ComputationalAgronomy http://10.205.1.14:5000/
--- sync_devices.py
+++ sync_devices.py
@@ -1,9 +1,21 @@
 import json
+import argparse
 
-streamdeck_config_import = "/home/steven/Documents/streamdeck_ui_for_import.json"
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", help="input file", required=True)
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("--lab_to_home", action="store_true", help="sync lab to home")
+group.add_argument("--home_to_lab", action="store_true", help="sync home to lab")
+
+args = parser.parse_args()
+
+print(args)
+streamdeck_config_import = "/home/steven/Documents/SD_ui_for_import.json"
 
 streamdeck_config = "/home/steven/Documents/streamdeck_ui_export_20240125.json"
-streamdeck_config = "/home/steven/Documents/streamdeck_ui_export.json"
+streamdeck_config = "/home/steven/Documents/streamdeck_ui_export_2.json"
+streamdeck_config = args.input
 
 # import json
 with open(streamdeck_config, "r") as f:
@@ -14,10 +26,13 @@ DECK_ID = {"home": "DL41L2A40477", "lab": "DL07L1A84326"}
 deck_home = data["state"]["DL41L2A40477"]
 deck_lab = data["state"]["DL07L1A84326"]
 
-
-HOME_TO_LAB = True
-if HOME_TO_LAB:
+if args.lab_to_home:
+    data["state"][DECK_ID["home"]] = data["state"][DECK_ID["lab"]]
+elif args.home_to_lab:
     data["state"][DECK_ID["lab"]] = data["state"][DECK_ID["home"]]
+
+# if HOME_TO_LAB:
+#     data["state"][DECK_ID["lab"]] = data["state"][DECK_ID["home"]]
 
 # LAB_TO_HOME = False
 # if LAB_TO_HOME:
@@ -26,5 +41,6 @@ if HOME_TO_LAB:
 # export json
 with open(streamdeck_config_import, "w") as f:
     json.dump(data, f, indent=4)
+
 
 
